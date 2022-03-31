3ad47cbd1aba5bfb1ed58c3df04b833c1934adff
anzosasuke <anzosasuke@gmail.com>
2022-03-31 17:53:28 -0500

    my_change

---
+++ twitter_search/stemmer.py
@@ -0,0 +1,35 @@
+import os
+import re
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+#Install nltk.download('punkt') too
+def token_stemmer(file):
+    tweets = []
+    filter_tweet = []
+    tweet = []
+    ps = PorterStemmer()
+    final_tweet = ""
+
+    with open("raw_tweets.txt", "r") as data:
+        contents = data.readlines()
+        for content in contents:
+            content = content.encode('ascii', 'ignore').decode('ascii')#remove all non-ascii characters
+            content = re.sub(r'http\S+', '',content)# removes urls
+            content = re.sub(r'❤️+','', content)
+            tweets.append(word_tokenize(content))
+        stop_words = stopwords.words('english')
+        new_words = ["!", "/", "#", ".", "@", " ", "[", "?","]","'", '"', ":", ";", ",", "-", "(", ")","``"] # some addition to the nltk stopwords
+        stop_words.extend(new_words)
+        for words in tweets:
+            filter_tweet.append([word for word in words if not word.lower() in stop_words])
+        for words in filter_tweet:
+            tweet.append([ps.stem(word) for word in words])
+        for list in tweet:
+            final_tweet += ' '.join(word for word in list)
+            final_tweet += "\n"
+
+        return(final_tweet)
+
+if __name__ == "__main__":
+    token_stemmer()
