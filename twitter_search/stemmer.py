923a65385a8719a982dfe800fc1d89033c8e0129
Sumit Bhattarai <bhatraisummit@gmail.com>
2022-05-01 00:22:22 -0500

    # Updated idf with posting list and dictionary, added document representation, query vector, cosine similarity calculation and cosine rank vector.

--- twitter_search/stemmer.py
+++ twitter_search/stemmer.py
@@ -3,7 +3,9 @@ import re
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-#Install nltk.download('punkt') too
+
+
+# Install nltk.download('punkt') too
 def token_stemmer():
     tweets = []
     filter_tweet = []
@@ -14,23 +16,21 @@ def token_stemmer():
     with open("raw_tweets.txt", "r") as data:
         contents = data.readlines()
         for content in contents:
-
             # content = content.encode('ascii', 'ignore').decode('ascii')#remove all non-ascii characters
-            content = re.sub(r'http\S+', '',content)# removes urls
-            content = re.sub(r'❤️+','', content)
+            content = re.sub(r'http\S+', '', content)  # removes urls
+            content = re.sub(r'❤️+', '', content)
             content = re.sub(r'[^\x00-\x7F]+', '', content)
-            content = re.sub(r'  ','',content)
+            content = re.sub(r'  ', '', content)
             # content = re.sub(r'\n','',content)
-
 
             tweets.append(word_tokenize(content))
         stop_words = stopwords.words('english')
-        new_words = ["!", "/", "#", ".", "@", " ", "[", "?","]","'", '"', ":", ";", ",", "-", "(", ")","``"] # some addition to the nltk stopwords
+        new_words = ["!", "/", "#", ".", "@", " ", "[", "?", "]", "'", '"', ":", ";", ",", "-", "(", ")",
+                     "``"]  # some addition to the nltk stopwords
         stop_words.extend(new_words)
         for words in tweets:
             filter_tweet.append([word for word in words if not word.lower() in stop_words])
         for words in filter_tweet:
-
             tweet.append([ps.stem(word) for word in words])
         tweet = list(filter(lambda x: x, tweet))
         # print(tweet)
@@ -39,9 +39,10 @@ def token_stemmer():
             final_tweet += "\n"
         print(final_tweet)
         # while("" in final_tweet) :
-            # final_tweet.remove("")
-        return(final_tweet)
+        # final_tweet.remove("")
+        return (final_tweet)
         # print(final_tweet)
+
 
 if __name__ == "__main__":
     token_stemmer()
--- vector_space_model/tfidf.py
+++ vector_space_model/tfidf.py
@@ -1,6 +1,7 @@
 import os
 import re
 import math
+import string
 from collections import Counter
 # converted_content = []
 
@@ -9,7 +10,6 @@ from collections import Counter
 # Returns Document Frequency
 def doc_freq(terms):
     DF = {}
-
     for count in range(len(terms)):
         term = terms[count]
         for word in term:
@@ -24,49 +24,154 @@ def doc_freq(terms):
     # print(DF)
     # print(DF['like'])
     return DF
-def unique(T):
 
+
+def unique(T):
     # insert the list to the set
     lset = set(T)
     # convert the set to the list
     unique_list = (list(lset))
-    # print(A)
     return unique_list
-def tf_idf(DF, terms):
+
+
+# Find length of the document vector
+def vector_length(column):
+    sum = 0
+    for i in column:
+        sum = sum + i ** 2
+    return sum ** (1 / 2)
+
+
+# Normalize document vector
+def normalize_vector(column):
+    vector_len = vector_length(column)
+    for i in range(len(column)):
+        column[i] = round(column[i] / vector_len, 3)
+
+
+# Find cosine similarity of two Normalized document vectors
+def cosine_sim(D1, D2):
+    sim_vector = 0
+    for i in range(len(D1)):
+        sim_vector += D1[i] * D2[i]
+    return sim_vector
+
+
+# Find column vector
+def column_vector(vector, i):
+    return [row[i] for row in vector]
+
+
+# Returns idf and posting_list and dictionary
+def idf(terms):
+    posting_list = {}
+    IDF = {}
+    dictionary = []
+    for docid in range(len(terms)):
+        term_set = unique(terms[docid])
+        for word in term_set:
+            try:
+                posting_list[word].add((docid, terms[docid].count(word)))
+            except:
+                posting_list[word] = {(docid, terms[docid].count(word))}
+        # sort DF
+        posting_list = dict(sorted(posting_list.items()))
+    for term in posting_list:
+        dictionary.append(term)
+        IDF[term] = round(math.log10(len(terms) / len(posting_list[term])), 3)
+    return IDF, posting_list, dictionary
+
+
+# Create Query Vector
+def query_vector(query, IDF):
+    # Remove lower and remove punctuation when finalized
+    query = query.lower()
+    # remove punctuations
+    query = query.translate(str.maketrans('', '', string.punctuation))
+    query_term = query.strip().split()
+    query_vector = []
+    for term in IDF:
+        query_vector.append(IDF[term] * query_term.count(term))
+    # Normalize Query Vector
+    normalize_vector(query_vector)
+    return query_vector
+
+
+# creates a vector model for a corpus (content of file)
+def document_representation(IDF, posting_list, no_of_docs):
+    row = len(IDF)
+    doc_rep = [[0 for i in range(no_of_docs)] for j in range(row)]
+    # Keep count of term id
+    k = 0
+    for posting in posting_list:
+        for (doc_id, count) in posting_list[posting]:
+            doc_rep[k][doc_id] = count * IDF[posting]
+        k += 1
+
+    # Normalize document representation
+    lengths_of_vector = []
+    for i in range(no_of_docs):
+        doc_vector = column_vector(doc_rep, i)
+        lengths_of_vector.append(vector_length(doc_vector))
+
+    for i in range(row):
+        for j in range(no_of_docs):
+            doc_rep[i][j] = round(doc_rep[i][j] / lengths_of_vector[j], 3)
+    return doc_rep
+
+
+# Print vectors
+def print_vectors(doc_representation, dictionary):
+    for col in range(len(doc_representation[0])):
+        print(f'\tD{col}', end='')
+    print()
+    for row in range(len(doc_representation)):
+        print(dictionary[row], end='\t')
+        for col in range(len(doc_representation[row])):
+            print(doc_representation[row][col], end='\t')
+        print()
+
+
+# Cosine Rank Vector/Dictionary from normalized query vector and normalized doc rep
+def cosine_rank(doc_representation, query_vector):
+    rank_dict = {}
+    for col in range(len(doc_representation[0])):
+        doc = f'D{col}'
+        cos_sim = round(cosine_sim(column_vector(doc_representation, col), query_vector), 3)
+        rank_dict[doc] = cos_sim
+    return dict(sorted(rank_dict.items(), key=lambda kv: kv[1], reverse= True))
+
+
+def tf_idf(IDF, terms):
     tfidf = {}
-    # tf = {}
     N = len(terms)
     for i in range(N):
         term = terms[i]
-        # count = 0
         for word in unique(term):
             tf = term.count(word)
-            df = DF[word]
-            # print("{} {}".format(word,df))
-            idf = math.log(N/(df+1)) #we cannot divide by 0, we smoothen the value by adding 1 to the denominator.
-            tfidf[i,word] = tf*idf
-            # print("{} {}".format(word,tf))
-    # print(tfidf)
+            idf = IDF[word]
+            # we cannot divide by 0, we smoothen the value by adding 1 to the denominator, No need. DF cannot be zero
+            tfidf[i, word] = tf * idf
     return tfidf
-
-
-
 
 
 with open("output.txt", "r") as data:
     contents = data.readlines()
     terms = []
     # print((converted_content))
-    no_of_documents = len(contents) #Counting the tweet as a single document
+    no_of_documents = len(contents)  # Counting the tweet as a single document
     for content in contents:
-        # print(content)
-        content = content.split()
-        # print(type(terms))
+        # conversion to lower case
+        content = content.lower()
+        # remove punctuations
+        content = content.translate(str.maketrans('', '', string.punctuation))
+        content = content.strip().split()
+        # for term in content:
         terms.append(content)
-    # print()
-    # print(len(terms))
-    DF1 = doc_freq(terms)
-    tf_idf(DF1, terms)
-
-    # print(contents.split())
-    # print(no_of_documents)
+    IDF, posting_list, dictionary = idf(terms)
+    doc_rep = document_representation(IDF, posting_list, len(terms))
+    print_vectors(doc_rep, dictionary)
+    print(column_vector(doc_rep, 1))
+    Q1 = 'Gold Silver Truck.'
+    qv = query_vector(Q1, IDF)
+    print(cosine_rank(doc_representation=doc_rep, query_vector=qv))
