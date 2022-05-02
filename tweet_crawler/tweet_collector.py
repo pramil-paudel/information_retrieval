c7cb086445c495f453f38609ea3e35466f4a5225
Sumit Bhattarai <bhatraisummit@gmail.com>
2022-05-01 23:40:04 -0500

    seach to use extended text instead of partial text

--- tweet_crawler/tweet_collector.py
+++ tweet_crawler/tweet_collector.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 # API Keys and Tokens
 consumer_key = ""
@@ -17,14 +18,20 @@ def remove_new_line(string_to_remove_new_line):
 
 
 def extract_top_tweets(file_to_write_raw_data):
-    places = api.geo_search(query="USA", granularity="country")
-    place_id = places[0].id
-    tweets = api.search(q="place:%s" % place_id, count=1000)
+    # places = api.search_geo(query="USA", granularity="country")
+    # print(places)
+    # #96683cc9126741d1
+    # place_id = places[0].id
+    place_id = '96683cc9126741d1'
+    tweets = api.search_tweets(q="place:%s" % place_id, count=1000, tweet_mode='extended')
+    # obj = json.loads(tweets)
+    # json_formatted_str = json.dumps(obj, indent=4)
+    # print(json_formatted_str)
     raw_data = open(file_to_write_raw_data, "w")
     document_name = 0
     for tweet in tweets:
         # raw_data.write(remove_new_line(str(document_name)) + "|" + tweet.text + "\n")
-        raw_data.write(remove_new_line(tweet.text) + "\n")
+        raw_data.write(remove_new_line(tweet.full_text) + "\n")
         document_name += 1
     raw_data.close()
 
